5757275d9a6ee63eb8a481a91666aabba1847aa8
samarendrabehera.papu@gmail.com <samarendrabehera.papu@gmail.com>
2024-06-23 12:16:11 +0530

    update

--- item/urls.py
+++ item/urls.py
@@ -6,4 +6,6 @@ app_name = 'item'
 urlpatterns = [
     path('new/', views.newItem, name='new_item'),
     path('<int:pk>/', views.detail, name='detail'),
+    path('<int:pk>/delete/', views.delete_item, name='delete'),
+    path('<int:pk>/edit/', views.edit_item, name='edit'),
 ]
--- item/views.py
+++ item/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 from .models import Item
 
 def detail(request, pk):
@@ -27,3 +27,32 @@ def newItem(request):
         'form': form,
         'title': "New Item"
     })
+
+
+@login_required
+def edit_item(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'Edit item'
+    })
+
+
+@login_required
+def delete_item(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    item.delete()
+
+    return redirect('dashboard:index')
