8a9dc71a4912ffa2c1e090de405fed27daeccf64
samarendrabehera.papu@gmail.com <samarendrabehera.papu@gmail.com>
2024-06-23 13:17:52 +0530

    update

--- item/views.py
+++ item/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
 from .forms import NewItemForm, EditItemForm
-from .models import Item
+from .models import Item, Category
+
+def items(request):
+    query = request.GET.get('query', '')
+    category_id = request.GET.get('category', 0)
+
+    categories = Category.objects.all()
+
+    items = Item.objects.filter(is_sold=False)
+
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    
+    if category_id:
+        items = items.filter(category=category_id)
+
+    return render(request, 'item/items.html', {
+        'items': items,
+        "query": query,
+        "categories": categories,
+        "category_id": int(category_id),
+    })
+
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
